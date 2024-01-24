2daff6f511c2bd0fb2d0e068d451cd253b366cc1
SeyyedMohammadAli Abyar <ue.abyar@gmail.com>
2024-01-24 22:39:02 +0330

    Now we can change background color by selecting cells.

--- src/train/changeBackColor.java
+++ src/train/changeBackColor.java
@@ -5,18 +5,13 @@ import java.io.File;
 
 import ir.sharif.math.bp02_1.hex_chess.graphics.Application;
 import ir.sharif.math.bp02_1.hex_chess.graphics.listeners.EventListener;
-import ir.sharif.math.bp02_1.hex_chess.graphics.models.HexagonCell;
-import ir.sharif.math.bp02_1.hex_chess.graphics.panel.BoardPanel;
-import ir.sharif.math.bp02_1.hex_chess.util.PieceName;
+
 
 public class changeBackColor implements EventListener {
     Application app;
     PiecePack data;
-    BoardPanel panel;
     int lrow;
     char lcol;
-    Color dtcolor;
-    String dPiece;
 
     public changeBackColor(Application app, PiecePack data) {
         this.app = app;
@@ -30,56 +25,7 @@ public class changeBackColor implements EventListener {
         }
         lrow = row;
         lcol = col;
-//        HexagonCell cell = new HexagonCell();
-//        cell.setBackGroundColor(Color.BLUE);
-        /*
-         * for (int i = 0; i < data.row.length; i++){
-         * try {
-         * if (row == data.row[i]) {
-         * for (int j = 0; j<data.col.length ; j++){
-         * if (col == data.col[j]) {
-         * System.out.println(data.row[i]);
-         * dPiece = data.name[i];
-         * dtcolor = data.tColor[i];
-         * System.out.println(dtcolor);
-         * app.setCellProperties(row, col, dPiece, Color.blue, dtcolor);
-         * app.setCellProperties(lrow, lcol, null, null, Color.white);
-         * lrow = row;
-         * lcol = col;
-         * }
-         * }
-         * 
-         * }
-         * } catch (Exception e) {
-         * // TODO: handle exception
-         * }
-         * }
-         
-        for (int i = 0; i < data.row.length; i++) {
-            try {
-                if (row == data.row[i]) {
-                    for (int j = 0; j < data.col.length; j++) {
-                        if (col == data.col[j]) {
-                            dPiece = data.piece[i];
-                            dtcolor = data.tColor[i];
-                            app.setCellProperties(row, col, dPiece, Color.blue, dtcolor);
-                            System.out.println(dPiece);
-                        }
-                    }
-                }if(row != data.row[i]){
-                    dPiece = null;
-                    dtcolor = null;
-                    app.setCellProperties(row, col, dPiece, Color.blue, dtcolor);
-                    app.setCellProperties(lrow, lcol, dPiece, null, dtcolor);
-                }
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
 
-        }
-        lrow = row;
-        lcol = col;
-        */
     }
 
     @Override
