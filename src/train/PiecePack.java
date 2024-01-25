9ee0428a79defc626c1db55aaeac09c902d21583
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-01-25 14:28:36 +0330

    terrrrrrrrrrrr!

--- src/train/PiecePack.java
+++ src/train/PiecePack.java
@@ -2,6 +2,9 @@ package train;
 
 import ir.sharif.math.bp02_1.hex_chess.util.PieceName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PiecePack {
     public int[] wrow = {1,1,1,1,1,1,1,1,1,2,3,2,3,4,5,4,3,2};
     public char[] wcol = {'b','c','d','e','f','g','h','i','k','f','f','c','d','e','f','g','h','i'};
@@ -10,6 +13,7 @@ public class PiecePack {
     public char[] bcol = {'b','c','d','e','f','g','h','i','k','f','f','c','d','e','f','g','h','i'};
     public String[] bpiece = {PieceName.BLACK_PAWN,PieceName.BLACK_ROCK,PieceName.BLACK_KNIGHT,PieceName.BLACK_QUEEN,PieceName.BLACK_BISHOP,PieceName.BLACK_KING,PieceName.BLACK_KNIGHT,PieceName.BLACK_ROCK,PieceName.BLACK_PAWN,PieceName.BLACK_BISHOP,PieceName.BLACK_BISHOP,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN,PieceName.BLACK_PAWN};
     public String[] pieces = {PieceName.BLACK_PAWN,PieceName.BLACK_ROCK,PieceName.BLACK_KNIGHT,PieceName.BLACK_QUEEN,PieceName.BLACK_BISHOP,PieceName.BLACK_KING,PieceName.BLACK_PAWN,PieceName.BLACK_ROCK,PieceName.BLACK_KNIGHT,PieceName.BLACK_QUEEN,PieceName.BLACK_BISHOP,PieceName.BLACK_KING};
+
     public int num = 18;
 
 }
--- src/train2/GameManager.java
+++ src/train2/GameManager.java
@@ -4,7 +4,9 @@ import ir.sharif.math.bp02_1.hex_chess.graphics.Application;
 import train.PiecePack;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GameManager {
@@ -24,8 +26,15 @@ public class GameManager {
     int lrow;
     char lcol;
     Color lBColor;
+    public List<Character> col = new ArrayList<>();
+    char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l'};
 
-    private void Move(Cell lcell, int lrow, char lcol, Cell ncell, int nrow, char ncol){
+
+    private void Move(Cell lcell, Cell ncell){
+        int nrow = ncell.getRow();
+        char ncol = ncell.getColumn();
+        int lrow = lcell.getRow();
+        char lcol = lcell.getColumn();
         app.setCellProperties(nrow, ncol, lcell.getPiece(), Color.BLUE, lcell.getTcolor());
         bBoard.put(""+nrow+ncol,new Cell(null, lcell.getPiece(), lcell.getTcolor(), nrow, ncol));
         bBoard.remove(""+lrow+lcol);
@@ -34,16 +43,18 @@ public class GameManager {
     }
     public void turn(){}
     private void removePiece(){}
-    private void changeBackgroundColor(Cell cell, int row, char col){
+    private void changeBackgroundColor(Cell cell, int row, char col, Color color){
+
         if (lBColor==Color.GREEN){
-            Move(lcell, lrow, lcol, cell, row, col);
+            Move(lcell, cell);
         }
         if (cell!=null){
             app.changeBackGround(row, col, Color.GREEN);
             lBColor = Color.GREEN;
         }else{
             app.changeBackGround(row, col, Color.blue);
-        }if ((row != lrow) || (col != lcol)){
+        }
+        if ((row != lrow) || (col != lcol)){
             app.changeBackGround(lrow, lcol, null);
         }
         lrow = row;
@@ -56,7 +67,7 @@ public class GameManager {
         this.column = column;
         try{
             sCell = bBoard.get(""+row+column);
-            changeBackgroundColor(sCell, row, column);
+            changeBackgroundColor(sCell, row, column, null);
             whatSelected(sCell);
         }catch (Exception e){
         }
@@ -68,11 +79,16 @@ public class GameManager {
             app.setCellProperties(pack.brow[i], pack.bcol[i], pack.bpiece[i], null, Color.BLACK);
             bBoard.put(""+pack.brow[i]+pack.bcol[i],new Cell(null, pack.bpiece[i], Color.BLACK, pack.brow[i], pack.bcol[i]));
         }
+        for(char c : chars){
+            col.add(c);
+        }
     }
     private void whatSelected(Cell cell){
         String piece = cell.getPiece();
-        if (piece.equals(pack.pieces[0])||piece.equals(pack.pieces[6])){
-            System.out.println(true);
+        if (piece.equals(pack.pieces[0])){
+            app.changeBackGround(cell.getRow(), chars[col.indexOf(cell.getColumn())+1], Color.darkGray);
+            app.changeBackGround(cell.getRow(), chars[col.indexOf(cell.getColumn())-1], Color.darkGray);
+
         }
     }
 }
